270bc8826b60fc122bfa4a281532d7bcc5748995
Maxim Samsonenko <samsonenko.max@gmail.com>
2021-12-22 20:34:02 +0200

    smth

--- src/api-images.js
+++ src/api-images.js
@@ -1,10 +1,10 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const ACCESS_KEY = '24444752-6eeb7e9783b35bc5419290dda';
-const SEARCH_OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=5';
+const SEARCH_OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
 import Notiflix from 'notiflix';
 
-export default class PicturesApi{
-  constructor(){
+export default class PicturesApi {
+  constructor() {
     this.searchQuery = '';
     this.page = 1;
   }
@@ -14,22 +14,23 @@ export default class PicturesApi{
     Notiflix.Loading.standard();
     return fetch(
       `${BASE_URL}?key=${ACCESS_KEY}&q=${this.searchQuery}&${SEARCH_OPTIONS}&page=${this.page}`,
-    ).then(response => response.json()).then(data =>{
-      
-      this.incrementPage();
-      return data;
-    });
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.incrementPage();
+        return data;
+      });
   }
-  incrementPage(){
+  incrementPage() {
     this.page += 1;
   }
-  resetPage(){
+  resetPage() {
     this.page = 1;
   }
-  get query(){
+  get query() {
     return this.searchQuery;
   }
-  set query(newQuery){
+  set query(newQuery) {
     this.searchQuery = newQuery;
   }
 }
