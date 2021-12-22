e3219b702b2d70dac8126323b9358c316b7c7910
samsonenkomax87 <samsonenko.max@gmail.com>
2021-12-22 15:30:08 +0200

    class added

--- src/api-images.js
+++ src/api-images.js
@@ -1,10 +1,35 @@
 const BASE_URL = 'https://pixabay.com/api/';
 const ACCESS_KEY = '24444752-6eeb7e9783b35bc5419290dda';
-const SEARCH_OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
+const SEARCH_OPTIONS = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=5';
+import Notiflix from 'notiflix';
 
-export function fetchPictures(searchQuery) {
-  let pageCount = 1;
-  return fetch(
-    `${BASE_URL}?key=${ACCESS_KEY}&q=${searchQuery}&${SEARCH_OPTIONS}&page=${pageCount}`,
-  ).then(response => response.json());
+export default class PicturesApi{
+  constructor(){
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchPictures(searchQuery) {
+    let pageCount = 1;
+    Notiflix.Loading.standard();
+    return fetch(
+      `${BASE_URL}?key=${ACCESS_KEY}&q=${this.searchQuery}&${SEARCH_OPTIONS}&page=${this.page}`,
+    ).then(response => response.json()).then(data =>{
+      
+      this.incrementPage();
+      return data;
+    });
+  }
+  incrementPage(){
+    this.page += 1;
+  }
+  resetPage(){
+    this.page = 1;
+  }
+  get query(){
+    return this.searchQuery;
+  }
+  set query(newQuery){
+    this.searchQuery = newQuery;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,50 +1,47 @@
-{
-  /* <div class="photo-card">
-  <img src="" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-    </p>
-  </div>
-</div>; */
-}
+
 import Notiflix from 'notiflix';
 
-import { fetchPictures } from './api-images';
+import  PicturesApi  from './api-images';
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-button');
 
+const pictures = new PicturesApi();
+
+console.log(pictures)
+
 form.addEventListener('submit', onFormSubmit);
+loadBtn.addEventListener('click', onBtnClick);
 
 function onFormSubmit(e) {
   e.preventDefault();
-  gallery.innerHTML = '';
-  const {
+const {
     elements: { searchQuery },
   } = e.currentTarget;
 
-  console.log(e.currentTarget);
-  console.log(searchQuery.value);
-  fetchPictures(searchQuery.value).then(pictureMarkup);
-  // loadBtn.addEventListener('click', () => {
-  //   fetchPictures(searchQuery.value).then(pictureMarkup);
-  // });
+  pictures.query = searchQuery.value;
+  gallery.innerHTML = '';
+  pictures.resetPage();
+  pictures.fetchPictures().then(result => {
+    showNotification(result);
+    pictureMarkup(result);
+  })
+  
+  // console.log(e.currentTarget);
+  // console.log(searchQuery.value);
+  // fetchPictures(searchQuery.value).then(pictureMarkup);
+}
+
+function onBtnClick(){
+  pictures.fetchPictures().then(pictureMarkup);
+  Notiflix.Loading.remove(500);
+
 }
 
 function pictureMarkup(photos) {
-  const { hits, totalHits } = photos;
-  Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+  console.log(photos)
+  const { hits, total } = photos;
   const markUp = hits
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
       return `<div class="photo-card">
@@ -71,5 +68,15 @@ function pictureMarkup(photos) {
     })
     .join('');
 
-  return gallery.insertAdjacentHTML('afterbegin', markUp);
+  return gallery.insertAdjacentHTML('beforeend', markUp);
+}
+function showNotification(result){
+  if(result.hits.length === 0){
+    return Notiflix.Notify.failure(`Sorry, there are ${result.total} images matching your search query. Please try again.`);
+    }
+    if(result.hits.length === 1){
+     return Notiflix.Notify.success(`Hooray! We found ${result.total} image.`);
+    }
+    Notiflix.Loading.remove(500);
+   return Notiflix.Notify.success(`Hooray! We found ${result.total} images.`);
 }
